21b206b0f6fbc3a1fe7ed88f35849c8cad509a7d
qqlaker <skeglz@mail.ru>
2023-09-08 07:59:19 +0300

    move txts to config/

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from requests.exceptions import TooManyRedirects
 
 def proxies_parse():
     proxies = []
-    useragents = open('useragents.TXT').read().split('\n')
+    useragents = open('config/useragents.TXT').read().split('\n')
     useragent = {'User-Agent': choice(useragents)}
     proxy = {'http': 'http://' + '191.101.39.193:80'}
     # hidemy.name outdated
@@ -22,16 +22,16 @@ def proxies_parse():
     return proxies
 
 def change_proxy(proxies):
-    useragents = open('useragents.TXT').read().split('\n')
+    useragents = open('config/useragents.TXT').read().split('\n')
     proxy = {'http': 'http://' + proxies.pop(0)}
     useragent = {'User-Agent': choice(useragents)}
     return proxy, useragent
 
 def main():
     proxies = []
-    names = open('marketnames.TXT').read().split('\n')
+    names = open('config/marketnames.TXT').read().split('\n')
     # proxies = proxies_parse()
-    with open('proxies.TXT', 'r') as p:
+    with open('config/proxies.TXT', 'r') as p:
         proxies.append(p.read())
     proxy, useragent = change_proxy(proxies)
     for i in range(1000):
